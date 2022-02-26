370ad26031f7577239659664b66b28fbe73b4ee0
Gitadmasai <97456833+Gitadmasai@users.noreply.github.com>
2022-02-27 01:19:32 +0530

    electronicPage done

---
+++ croma product page/electronicData.js
@@ -0,0 +1,454 @@
+var electronicsData = [{
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1623390037/Croma%20Assets/Communication/Mobiles/Images/235154_sbzk7f.png/mxw_1440,f_auto",
+    name: "OnePlus Nord CE 5G (256GB ROM, 12GB RAM, Blue Void)",
+    brand: "OnePlus",
+    price: "26999",
+    strikedOffPrice: "29999.00",
+    type: "mobile",
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1638452106/Croma%20Assets/Communication/Mobiles/Images/233910_yvmohs.png/mxw_1440,f_auto",
+    name: "OnePlus 9R (128GB ROM, 8GB RAM, LE2101, Carbon Black)",
+    brand: "OnePlus",
+    price: "39999",
+    strikedOffPrice: "41999.00",
+    type: "mobile",
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1606069737/Croma%20Assets/Communication/Mobiles/Images/8992315899934.png/mxw_1440,f_auto",
+    name: "Samsung Galaxy S20 FE (128GB ROM, 8GB RAM, G780FN, Cloud Mint)",
+    brand: "Samsung",
+    price: "37999",
+    strikedOffPrice: "41999.00",
+    type: "mobile",
+
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/h_300,w_300/v1639480276/Croma%20Assets/Communication/Mobiles/Images/242288_hvuaav.png/mxw_1440,f_auto",
+    name: "Samsung Galaxy Z Fold3 5G (256GB ROM, 12GB RAM, SM-F926BZSD, Phantom Silver)",
+    brand: "Samsung",
+    price: "149999",
+    strikedOffPrice: "159999.00",
+    type: "mobile",
+
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/h_300,w_300/v1643703628/Croma%20Assets/Communication/Mobiles/Images/225543_ofd6jd.png/mxw_1440,f_auto",
+    name: "Samsung Galaxy M21 (64GB ROM, 4GB RAM, Light Blue)",
+    brand: "OnePlus",
+    price: "12999",
+    strikedOffPrice: "15999.00",
+    type: "mobile",
+
+}, {
+    imageUrl: "https://media.croma.com/image/upload/v1644503735/Croma%20Assets/Computers%20Peripherals/Laptop/Images/246903_i5zkd2.png",
+    name: "Asus E410KA-EK001W Celeron Windows 11 Home Laptop (4GB RAM, 256GB SSD, Intel HD Graphics, 35.56cm, 90NB0UA1-M02640, Peacock Blue)",
+    brand: "Asus",
+    price: "26999",
+    strikedOffPrice: "35999.00",
+    type: "tv",
+
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1642492381/Croma%20Assets/Computers%20Peripherals/Laptop/Images/247391_wfnctw.png/mxw_1440,f_auto",
+    name: "Dell Inspiron 3515 Ryzen 5 Windows 11 Notebook (8GB RAM, 1TB HDD, 256GB SSD, AMD Radeon Graphics, MS Office, 39.62cm, D560706WIN9S, Platinum Silver)",
+    brand: "Dell",
+    price: "51999",
+    strikedOffPrice: "62999.00",
+    type: "tv",
+
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1632890004/Croma%20Assets/Computers%20Peripherals/Laptop/Images/243937_hbqok9.png/mxw_1440,f_auto",
+    name: "Dell Inspiron 3511 11th Gen Core i3 Windows 10 Laptop (8 GB RAM, 512GB SDD, Intel UMA Graphics, MS Office, 39.62cm, D560576WIN9S, Platinum Silver)",
+    brand: "Dell",
+    price: "47472",
+    strikedOffPrice: "51348.00",
+    type: "tv",
+
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1644911526/Croma%20Assets/Computers%20Peripherals/Laptop/Images/248683_u0w2u0.png/mxw_1440,f_auto",
+    name: "Microsoft Surface Studio 11th Gen Core i7 Windows 11 2-in-1 Laptop (16GB RAM, 512GB SSD, NVIDIA GeForce RTX 3050 Ti + 4GB Graphics, MS Office, 36.57cm, A1Y-00022, Platinum)",
+    brand: "Microsoft",
+    price: "209990",
+    strikedOffPrice: "215999.00",
+    type: "tv",
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1639212507/Croma%20Assets/Computers%20Peripherals/Laptop/Images/245442_vmrr92.png/mxw_1440,f_auto",
+    name: "Asus 14 EK322TS Rayzen 3 Windows 10 Home Laptop (8GB RAM, 256GB SSD, AMD Radeon + Shared Memory, MS Office, 35.5cm, 90NB0T31-M11760, Silver)",
+    brand: "Asus",
+    price: "35490",
+    strikedOffPrice: "46990.00",
+    type: "tv",
+
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1631084878/Croma%20Assets/Entertainment/Television/Images/242944_e5jvij.png/mxw_1440,f_auto",
+    name: "Croma Fire TV 80cm (32 Inch) HD Ready LED Smart TV (Alexa Voice Assistant Remote, CREL7364, Black)",
+    brand: "Croma",
+    price: "13999",
+    strikedOffPrice: "25999.00",
+    type: "tv",
+
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1641446371/Croma%20Assets/Entertainment/Television/Images/246263_ukaju8.png/mxw_2048,f_auto",
+    name: "Croma Fire TV 127cm (50 Inch) Ultra HD 4K LED Smart TV (3 Years Warranty, Alexa Voice Assistant Remote, CREL7367, Black)",
+    brand: "Croma",
+    price: "36999",
+    strikedOffPrice: "60999.00",
+    type: "tv",
+
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1606073956/Croma%20Assets/Entertainment/Television/Images/9007409725470.png/mxw_1440,f_auto",
+    name: "Samsung Series 4 T4350 80 cm (32 inch) HD Ready LED Smart TV (UA32T4350AKXXL, Black)",
+    brand: "Samsung",
+    price: "21790",
+    strikedOffPrice: "22900.00",
+    type: "tv",
+
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/h_300,w_300/v1618648791/Croma%20Assets/Entertainment/Television/Images/234144_bpl1rp.png/mxw_1440,f_auto",
+    name: "Samsung 4 Series 80cm (32 Inch) HD Ready LED Smart TV (Wi-Fi Supported, UA32T4450AKLXL, Titan Grey)",
+    brand: "Samsung",
+    price: "22190",
+    strikedOffPrice: "23900.00",
+    type: "tv",
+
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1638446751/Croma%20Assets/Communication/Mobiles/Images/233912_biu7qr.png/mxw_1440,f_auto",
+    name: "OnePlus 9R (128GB ROM, 8GB RAM, LE2101, Lake Blue)",
+    brand: "OnePlus",
+    price: "39999",
+    strikedOffPrice: "45999.00",
+    type: "mobile",
+
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1644497504/Croma%20Assets/Communication/Mobiles/Images/248228_ryile9.png/mxw_1440,f_auto",
+    name: "Vivo T1 5G (128GB ROM, 4GB RAM, PD2165CF_IN, Rainbow Fantasy)",
+    brand: "Vivo",
+    price: "15990",
+    strikedOffPrice: "21999.00",
+    type: "mobile",
+
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1609746509/Croma%20Assets/Communication/Mobiles/Images/231590_dknho3.png/mxw_1440,f_auto",
+    name: "Vivo Y20A (64GB ROM, 3GB RAM, Dawn White)",
+    brand: "Vivo",
+    price: "11999",
+    strikedOffPrice: "16999.00",
+    type: "mobile",
+
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1623390037/Croma%20Assets/Communication/Mobiles/Images/235154_sbzk7f.png/mxw_1440,f_auto",
+    name: "OnePlus Nord CE 5G (256GB ROM, 12GB RAM, Blue Void)",
+    brand: "OnePlus",
+    price: "26,999",
+    strikedOffPrice: "29,999.00",
+    type: "mobile",
+
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1605305714/Croma%20Assets/Communication/Mobiles/Images/8958086086686.png/mxw_1440,f_auto",
+    name: "Vivo X50 (Glaze Black, 256 GB, 8 GB RAM)",
+    brand: "Vivo",
+    price: "37990",
+    strikedOffPrice: "43990.00",
+    type: "mobile",
+
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1605302753/Croma%20Assets/Communication/Mobiles/Images/8986320142366.png/mxw_1440,f_auto",
+    name: "Nokia 5310 (16MB ROM, 8MB RAM, White)",
+    brand: "Nokia",
+    price: "3499",
+    strikedOffPrice: "5578.00",
+    type: "mobile",
+
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1605328225/Croma%20Assets/Communication/Mobiles/Images/8944737353758.png/mxw_1440,f_auto",
+    name: "Nokia 5310 (16MB ROM, 8MB RAM, Black)",
+    brand: "Nokia",
+    price: "3299",
+    strikedOffPrice: "5578.00",
+    type: "mobile",
+
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1605197456/Croma%20Assets/Communication/Mobiles/Images/8943046950942.png/mxw_1440,f_auto",
+    name: "Nokia 105 (4MB ROM, 4MB RAM, Black)",
+    brand: "Nokia",
+    price: "978",
+    strikedOffPrice: "2022.00",
+    type: "mobile",
+
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1632294355/Croma%20Assets/Communication/Mobiles/Images/243366_r9vqfd.png/mxw_1440,f_auto",
+    name: "Nokia G10 (64GB ROM, 4GB RAM, Dusk)",
+    brand: "Nokia",
+    price: "11499",
+    strikedOffPrice: "14999.00",
+    type: "mobile",
+
+}, {
+    imageUrl: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1606483369/Croma%20Assets/Computers%20Peripherals/Laptop/Images/9000567668766.png/mxw_1440,f_auto",
+    name: "Asus ExpertBook B9 (B9450FA-BM0696R) Core i7 10th Gen Windows 10 Pro Laptop",
+    brand: "Nokia",
+    price: "167994",
+    strikedOffPrice: "212375.00",
+    type: "mobile",
+
+}, ];
+
+
+
+
+// /Adding the script tag
+// 
+var cartarr = JSON.parse(localStorage.getItem("cartarr")) || []
+
+displayData(electronicsData)
+
+function handlePricesort() {
+    var selected = document.querySelector("#pricesort").value;
+    if (selected == "high") {
+        ele_data.sort(function(a, b) {
+            return Number(b.price) - Number(a.price);
+        })
+    }
+    if (selected == "low") {
+        electronicsData.sort(function(a, b) {
+            return Number(a.price) - Number(b.price)
+        })
+    }
+    displayData(electronicsData)
+}
+
+
+function filterCat() {
+    var selected = document.querySelector("#brandfilter").value;
+    console.log(selected)
+
+    var filteredlist = electronicsData.filter(function(elem) {
+        return elem.brand == selected;
+    })
+
+    displayData(filteredlist)
+}
+
+
+
+
+
+
+function displayData(ele_data) {
+    console.log(ele_data)
+    document.querySelector(".items").innerHTML = "";
+    ele_data.map(function(data) {
+        var div = document.createElement("div")
+        div.setAttribute("id", "flex");
+
+        var div1 = document.createElement("div")
+        div1.setAttribute("id", "d1")
+
+        var image = document.createElement("img")
+        image.setAttribute("src", data.imageUrl)
+        image.setAttribute("id", "images")
+
+        div1.append(image)
+
+        var div2 = document.createElement("div")
+        div2.setAttribute("id", "d2")
+
+        var name = document.createElement("h2");
+        name.textContent = data.name
+
+        var id = document.createElement("p")
+        id.textContent = "Product Id:" + " " + Math.round(Math.random() * 203145)
+        id.setAttribute("id", "id")
+
+
+
+        var div4 = document.createElement("div");
+        div4.setAttribute("id", "id1")
+
+        var p1 = document.createElement("h2");
+        p1.textContent = "₹" + Number(data.price).toLocaleString("en-IN")
+        p1.style.color = "white"
+
+        var p2 = document.createElement("h5");
+        p2.textContent = "₹" + data.strikedOffPrice
+        p2.style.color = "gray"
+        p2.style.textDecoration = "line-through"
+        p2.setAttribute("id", "p2")
+
+        var btn3 = document.createElement("button")
+        btn3.textContent = Math.round(Math.random() * 40) + "%"
+        btn3.setAttribute("id", "btn3")
+
+        var brand = document.createElement("h1");
+        brand.textContent = data.brand
+        brand.style.color = "white"
+
+        var div5 = document.createElement("div")
+        div5.setAttribute("id", "id2")
+
+        var ch = document.createElement("h4")
+        ch.textContent = "COMPARE";
+        ch.style.color = "white"
+
+        var c = document.createElement("h4")
+        c.textContent = "CONTECT TO STORE"
+        c.style.color = "white"
+
+        var free = document.createElement("p")
+        free.textContent = "Free Delivery by"
+        free.style.color = "white"
+
+        var free1 = document.createElement("p")
+        free1.textContent = "Tomorrow 400049"
+        free1.style.color = "rgb(11, 156, 117)"
+        free1.style.marginTop = "-10px"
+
+        var div6 = document.createElement("div")
+        div6.setAttribute("id", "id3")
+
+        var btn = document.createElement("button");
+        btn.textContent = "BUY NOW";
+        btn.style.cursor = "pointer"
+        btn.setAttribute("id", "btn")
+
+        var btn1 = document.createElement("button")
+        btn1.textContent = "ADD TO CART"
+        btn1.style.cursor = "pointer"
+        btn1.setAttribute("id", "btn1")
+        btn1.addEventListener("click", function() {
+            addtocart(data)
+        })
+
+        function addtocart(data) {
+            console.log(data)
+            cartarr.push(data);
+            document.querySelector(".cartcount").textContent = cartarr.length;
+            localStorage.setItem("cartarr", JSON.stringify(cartarr))
+            alert("Succecfull")
+                // window.location.href = "Cart.html"
+        }
+
+        div6.append(btn, btn1)
+
+        div5.append(ch, c)
+
+        div4.append(p1, p2, btn3)
+
+        div2.append(name, id, div4, brand, div5, free, free1, div6, )
+
+        div.append(div1, div2);
+
+        document.querySelector(".items").append(div)
+
+    })
+
+
+
+
+
+
+}
+//updating the car count
+
+document.querySelector(".cartcount").textContent = cartarr.length;
+
+document.querySelector(".cart").addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = `../layouts/index.html`
+})
+
+
+
+
+
+
+
+
+
+
+document.getElementById("underlinetext1").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("underlinetext2").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("underlinetext3").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("underlinetext4").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("underlinetext5").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("underlinetext6").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("underlinetext7").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("underlinetext8").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("underlinetext9").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("underlinetext10").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("underlinetext11").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("blackicon1").addEventListener("click", function() {
+    window.location.href = "https://www.facebook.com/CromaRetail/"
+})
+document.getElementById("blackicon2").addEventListener("click", function() {
+    window.location.href = "https://twitter.com/cromaretail?s=11"
+})
+document.getElementById("blackicon3").addEventListener("click", function() {
+    window.location.href = "https://www.youtube.com/user/CromaRetail"
+})
+document.getElementById("blackicon4").addEventListener("click", function() {
+    window.location.href = "https://www.instagram.com/croma.retail/"
+})
+document.getElementById("blackicon5").addEventListener("click", function() {
+    window.location.href = "https://www.pinterest.com/cromaretail/"
+})
+document.getElementById("blackicon1").addEventListener("click", function() {
+    window.location.href = "https://www.facebook.com/CromaRetail/"
+})
+document.getElementById("footerwidetext1").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("footerwidetext2").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("footerwidetext3").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("footerwidetext4").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("footerwidetext5").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("footerwidetext6").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("footerwidetext7").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("footer5widetext1").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("footer5widetext2").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("footer5widetext3").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("footer5widetext4").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("footer5widetext5").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
+document.getElementById("footerbox5heading").addEventListener("click", function() {
+    window.location.href = "products.html"
+})
